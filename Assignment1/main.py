0dfdfd7e424a70dfecb950f1159435e359f0a32e
Gautam Manek <gautam007@e.ntu.edu.sg>
2022-01-29 01:38:41 +0800

    feat: added cmv condition 11

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,4 +1,14 @@
-def main():
+def cmv_condition_11(points, G_PTS):
+    if G_PTS > len(points) - 2 or len(points) < 3:
+        return False
+    for beginIdx in range(len(points) - G_PTS - 1):
+        endIdx = beginIdx + G_PTS + 1
+        x_begin, x_end = points[beginIdx][0], points[endIdx][0]
+        if x_end - x_begin < 0:
+            return True
+    return False
+
 
 if __name__ == "__main__":
-    main()
+    pass
+
--- Assignment1/test.py
+++ Assignment1/test.py
@@ -1,4 +1,20 @@
-def main():
+import unittest
+from main import *
 
-if __name__ == "__main__":
-    main()
+class CMVCondition11TestCase(unittest.TestCase):
+    # begin and end points are the same, and are G_PTS = 2 apart
+    def test_false_1(self):
+        points = [(1, 2), (4, 2), (3, 1), (1, 2)] 
+        self.assertFalse(cmv_condition_11(points, G_PTS=2))
+    # increasing sequence of x-coordinates
+    def test_false_2(self):
+        points = [(1, 2), (2, 3), (3, 1), (4, 2), (5, 2)] 
+        self.assertFalse(cmv_condition_11(points, G_PTS=2))
+    # x-coordinate of (-1, 2) < (1, 2)
+    def test_true_1(self):
+        points = [(1, 2), (4, 2), (3, 1), (-1, 2)] 
+        self.assertTrue(cmv_condition_11(points, G_PTS=2))
+    # x -coordinate of (-1, 2) < (4, 2)
+    def test_true_2(self):
+        points = [(1, 2), (4, 2), (3, 1), (-1, 2)] 
+        self.assertTrue(cmv_condition_11(points, G_PTS=1))
