afab9796b94f10d789cd05abdfc73d24334c4640
dajamante <aimaiga2@gmail.com>
2022-01-28 14:26:04 +0100

    feat: check cmv condition 3, includes test

--- Assignment1/test.py
+++ Assignment1/test.py
@@ -1,4 +1,27 @@
-def main():
+import unittest
+from main import *
 
-if __name__ == "__main__":
-    main()
+
+class CMVCondition3TestCase(unittest.TestCase):
+    def test_area1_bigger_than_area(self):
+        """
+        Internal area of 3 consecutive datapoints 
+        are smaller than than AREA1.
+        """
+        points = [(2, 4), (1, -6), (5, 8)]
+        self.assertFalse(cmv_condition_3(points, 20))
+
+    def test_area1_smaller_than_area(self):
+        """
+        Internal area of 3 consecutive datapoints 
+        are bigger than than AREA1.
+        """
+        points = [(2, 4), (1, -6), (5, 8)]
+        self.assertTrue(cmv_condition_3(points, 5))
+
+    def test_area1_equals_zero(self):
+        """
+        AREA1 should not be 0 or negative
+        """
+        points = [(2, 4), (1, -6), (5, 8)]
+        self.assertFalse(cmv_condition_3(points, 0))
