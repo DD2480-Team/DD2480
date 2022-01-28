529e84957e1d4c8aac903b290b57cd992bd61ba1
Joel Lützow <lutzow.joel@gmail.com>
2022-01-28 12:46:23 +0100

    feat: check CMV condition 0, includes test

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,4 +1,26 @@
-def main():
+import math
 
-if __name__ == "__main__":
-    main()
+def main():
+    if __name__ == "__main__":
+        main()
+
+
+def check_condition_0(point_array, length1):
+    """ Checks if there is a set of consecutive data points
+        with a distance greater than a set length apart
+
+    Args:
+        point_array (type: list of 2-tuples): contains (x,y) values
+        length1 (type: float): minimum distance necessary
+
+    Returns:
+        (type: boolean) True if condition met, false otherwise
+    """
+    if(length1 < 0 or len(point_array) < 2): 
+        return False
+    for i in range(len(point_array) -1):
+        xdist = abs(point_array[i][0] - point_array[i+1][0])
+        ydist = abs(point_array[i][1] - point_array[i+1][1])
+        if(math.sqrt(xdist**2 + ydist**2 ) > length1):
+            return True
+    return False
--- Assignment1/test.py
+++ Assignment1/test.py
@@ -1,4 +1,42 @@
-def main():
+from main import check_condition_0
 
-if __name__ == "__main__":
-    main()
+def main():
+    if __name__ == "__main__":
+        main()
+
+#must exist one set of two consecutive data points that are further than length1
+#pass two points with dist 5 and and length1 4.5, should return True
+def test1_check_condition_0():
+    points = [(0,0),(4,3)]  #dist is 5
+    length1 = 4.5
+    assert(check_condition_0(points, length1) == True)
+    print("Success: test1_check_condition_0")
+
+#must exist one set of two consecutive data points that are further than length1
+#pass two points with dist 5 and and length1 5.5, should return False
+def test2_check_condition_0():
+    points = [(0,0),(4,3)]  #dist is 5
+    length1 = 5.5
+    assert(check_condition_0(points, length1) == False)
+    print("Success: test2_check_condition_0")
+
+#must exist one set of two consecutive data points that are further than length1
+#pass only one point, should return False
+def test3_check_condition_0():
+    points = [(0,0)]  #dist undefined
+    length1 = 0
+    assert(check_condition_0(points, length1) == False)
+    print("Success: test3_check_condition_0")
+
+#must exist one set of two consecutive data points that are further than length1
+#pass negative distance, should return False
+def test4_check_condition_0():
+    points = [(0,0)]  #dist undefined
+    length1 = 0
+    assert(check_condition_0(points, length1) == False)
+    print("Success: test4_check_condition_0")
+
+test1_check_condition_0()
+test2_check_condition_0()
+test3_check_condition_0()
+test4_check_condition_0()
