731ca7f07c20ff111976b255d0569f89f98c66e3
Jacob Mimms <jacobmimms@gmail.com>
2022-01-30 19:13:01 +0100

    fix: adjusted the tests for cmv condition 8

--- Assignment1/test.py
+++ Assignment1/test.py
@@ -5,19 +5,11 @@ from main import *
 class CMVCondition8TestCase(unittest.TestCase):
     def test_points_pass_with_distance_2_begining(self):
         """
-        The 3 first points should pass the test.
+        False since all points should fall inside radius 10 circle 
         """
         points = [(1, 0), (3, 0), (5, 0), (-1, -2), (2, 1),
                   (2, 2), (3, 3), (-2, -2), (1, 1), (-1, -1)]
-        self.assertTrue(cmv_condition_8(points, 2, 2, 1.5))
-
-    def test_points_pass_with_distance_2_end(self):
-        """
-        The list is processed and the 3 last points should pass the test.
-        """
-        points = [(-1, -2), (2, 1), (2, 2), (3, 3), (-2, -2),
-                  (1, 1), (-1, -1), (1, 0), (3, 0), (5, 0)]
-        self.assertTrue(cmv_condition_8(points, 2, 2, 1.5))
+        self.assertFalse(cmv_condition_8(points, 2, 2, 10, 10))
 
     def test_points_contained_by_radius1(self):
         """
@@ -25,30 +17,21 @@ class CMVCondition8TestCase(unittest.TestCase):
         """
         points = [(-1, -2), (2, 1), (2, 2), (3, 3), (-2, -2),
                   (1, 1), (-1, -1), (1, 0), (3, 0), (5, 0)]
-        self.assertFalse(cmv_condition_8(points, 2, 2, 9))
+        self.assertFalse(cmv_condition_8(points, 2, 2, 9, 10))
 
     def test_points_pass_with_distance_2_on_y_axis(self):
         """
         The list is processed and the a and b pts are checked on y axis
         """
-        points = [(-1, -2), (2, 1), (2, 2), (3, 3), (-2, -2),
-                  (1, 1), (-1, -1), (0, 1), (0, 3), (0, 5)]
-        self.assertTrue(cmv_condition_8(points, 2, 2, 1.5))
+        points = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0,6)]
+        self.assertTrue(cmv_condition_8(points, 2, 2, 2.99999, 7))
 
     def test_list_too_short(self):
         """
         The list has less than 5 points
         """
-        points = [(-1, -2), (2, 1), (2, 2)]
-        self.assertFalse(cmv_condition_8(points, 2, 2, 1.5))
-
-    def test_points_contained_by_large_radius(self):
-        """
-        The points should be contained by a radius 15 pts
-        """
-        points = [(1, 0), (3, 0), (5, 0), (-1, -2), (2, 1),
-                  (2, 2), (3, 3), (-2, -2), (1, 1), (-1, -1)]
-        self.assertFalse(cmv_condition_8(points, 2, 2, 15))
+        points = [(-1, -2), (2, 1), (2, 2), (3, 3)]
+        self.assertFalse(cmv_condition_8(points, 1, 1, 1.5, 4))
 
     def test_a_pts_b_pts_too_small(self):
         """
@@ -56,12 +39,15 @@ class CMVCondition8TestCase(unittest.TestCase):
         """
         points = [(1, 0), (3, 0), (5, 0), (-1, -2), (2, 1),
                   (2, 2), (3, 3), (-2, -2), (1, 1), (-1, -1)]
-        self.assertFalse(cmv_condition_8(points, 0, 0, 5))
+        self.assertFalse(cmv_condition_8(points, 0, 0, 5, 10))
 
-    def test_a_pts_b_pts_too_small(self):
+    def test_a_pts_b_pts_too_large(self):
         """
         It should be A PTS+B PTS ≤ (NUMPOINTS−3)
         """
         points = [(1, 0), (3, 0), (5, 0), (-1, -2), (2, 1),
                   (2, 2), (3, 3), (-2, -2), (1, 1), (-1, -1)]
-        self.assertFalse(cmv_condition_8(points, 15, 19, 5))
+        self.assertFalse(cmv_condition_8(points, 15, 19, 5, 10))
+
+if __name__ == "__main__":
+    unittest.main()
