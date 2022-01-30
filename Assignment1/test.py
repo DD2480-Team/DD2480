078dc1ecdfe0bb249eeec30eaef6ac971a93d084
Jacob Mimms <jacobmimms@gmail.com>
2022-01-30 20:56:24 +0100

    fix: adjusted tests for cmv condition 13

--- Assignment1/test.py
+++ Assignment1/test.py
@@ -2,18 +2,7 @@ import unittest
 from main import *
 
 
-class CMVCondition8TestCase(unittest.TestCase):
-
-    """[summary]
-    """
-
-    def test_points_pass_with_distance_2_begining(self):
-        """
-        The 3 first points should pass the test. Distances 2 and 2 are exceeding radius 1 and 2
-        """
-        points = [(1, 0), (3, 0), (5, 0), (-1, -2), (2, 1),
-                  (2, 2), (3, 3), (-2, -2), (1, 1), (-1, -1)]
-        self.assertTrue(cmv_condition_8(points, 2, 2, 1.5, 1))
+class CMVCondition13TestCase(unittest.TestCase):
 
     def test_points_fail_with_distance_2_begining(self):
         """
@@ -21,28 +10,31 @@ class CMVCondition8TestCase(unittest.TestCase):
         """
         points = [(1, 0), (3, 0), (5, 0), (-1, -2), (2, 1),
                   (2, 2), (3, 3), (-2, -2), (1, 1), (-1, -1)]
-        self.assertFalse(cmv_condition_8(points, 2, 2, 5, 6))
+        self.assertFalse(cmv_condition_13(points, 2, 2, 5, 6, len(points)))
 
     def test_points_fail_on_radius1(self):
         """
-        Radius 1 should make the test fail
+        Radius2 = 1 should make the test fail
         """
         points = [(1, 0), (3, 0), (5, 0), (-1, -2), (2, 1),
                   (2, 2), (3, 3), (-2, -2), (1, 1), (-1, -1)]
-        self.assertFalse(cmv_condition_8(points, 2, 2, 5, 1))
+        self.assertFalse(cmv_condition_13(points, 2, 2, 5, 1, len(points)))
 
     def test_points_fail_on_radius2(self):
         """
-        Radius 2 should make the test fail
+        no points aren't contained in radius 2 circle, but some do fall within radius 12
         """
         points = [(1, 0), (3, 0), (5, 0), (-1, -2), (2, 1),
                   (2, 2), (3, 3), (-2, -2), (1, 1), (-1, -1)]
-        self.assertFalse(cmv_condition_8(points, 2, 2, 1, 12))
+        self.assertTrue(cmv_condition_13(points, 2, 2, 2, 12, len(points)))
 
     def test_not_enough_points(self):
         points = [(1, 0), (3, 0), (5, 0)]
-        self.assertFalse(cmv_condition_8(points, 2, 2, 1.5, 1))
+        self.assertFalse(cmv_condition_13(points, 1, 1, 1.5, 1, len(points)))
 
     def test_radius2_is_zero(self):
         points = [(1, 0), (3, 0), (5, 0)]
-        self.assertFalse(cmv_condition_8(points, 2, 2, 1.5, 0))
+        self.assertFalse(cmv_condition_13(points, 1, 1, 1.5, 0, len(points)))
+
+if __name__ == "__main__":
+    unittest.main()
