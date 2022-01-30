8eb27b3d893e0166798cd225deab81db2d2c2896
Jacob Mimms <jacobmimms@gmail.com>
2022-01-30 19:12:33 +0100

    fix: changed iteration range from (numpoints - a_pts - b_pts - 3) to (... -2)

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -25,16 +25,15 @@ def cmv_condition_8(points, a_pts, b_pts, radius1, numpoints):
 
     if (a_pts < 1 or b_pts < 1):
         return False
-    if ((a_pts + b_pts) > len(points)-3):
+    if ((a_pts + b_pts) > numpoints-3):
         return False
 
     """
     explanation of indices:
     take array of length 6: [1,0,1,0,0,1], A_PTS = 1, B_PTS = 2
     need to check the points at index i, i+A_PTS+1, and i+A_PTS+1+B_PTS+1
-    the last value for i you can check in this example is 0 which is numpoints - A_PTS - B_PTS - 3
     """
-    for i in range(numpoints - a_pts - b_pts - 3):
+    for i in range(numpoints - a_pts - b_pts - 2):
         p1 = points[i]
         p2 = points[i+a_pts+1]
         p3 = points[i+a_pts+b_pts+2]
@@ -42,10 +41,10 @@ def cmv_condition_8(points, a_pts, b_pts, radius1, numpoints):
         x1, y1 = p1[0], p1[1]
         x2, y2 = p2[0], p2[1]
         x3, y3 = p3[0], p3[1]
-
         # Same as calculations as cmv_1
         center = [(x1 + x2 + x3) / 3, (y1 + y2 + y3) / 3]
-
+        print(a_pts, b_pts, "\n")
+        print(p1, p2, p3, "\n \n ")
         def distance_exceeds_radius(x, y):
             squared_distance = (x - center[0]) ** 2 + \
                 (y - center[1]) ** 2
