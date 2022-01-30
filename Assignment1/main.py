413529e1b9ebe673b7a9a4417a946842434c85b5
Jacob Mimms <jacobmimms@gmail.com>
2022-01-30 20:56:07 +0100

    fix: fixed logic for cmv condition 13, and added condition 8 to make writing easier

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,7 +1,59 @@
 import math
 
 
-def cmv_condition_8(points, a_pts, b_pts, radius1, radius2):
+def cmv_condition_8(points, a_pts, b_pts, radius1, numpoints):
+    """
+    There exists at least one set of three data points separated by exactly 
+    A PTS and B PTS consecutive intervening points, respectively, that cannot 
+    be contained within or on a circle of radius RADIUS1. 
+    The condition is not met when NUMPOINTS < 5.
+    1≤A PTS,1≤B PTS
+    A PTS+B PTS ≤ (NUMPOINTS−3)
+    Args:
+        points (list): coordinates
+        a_pts (int): number of interevening points
+        b_pts (int): number of inttervening points
+        radius1 (float): circle radius
+    Returns:
+        True if conditions are met
+    """
+    if numpoints < 5:
+        return False
+
+    if (a_pts < 1 or b_pts < 1):
+        return False
+    if ((a_pts + b_pts) > numpoints-3):
+        return False
+
+    """
+    explanation of indices:
+    take array of length 6: [1,0,1,0,0,1], A_PTS = 1, B_PTS = 2
+    need to check the points at index i, i+A_PTS+1, and i+A_PTS+1+B_PTS+1
+    """
+    for i in range(numpoints - a_pts - b_pts - 2):
+        p1 = points[i]
+        p2 = points[i+a_pts+1]
+        p3 = points[i+a_pts+b_pts+2]
+
+        x1, y1 = p1[0], p1[1]
+        x2, y2 = p2[0], p2[1]
+        x3, y3 = p3[0], p3[1]
+        # Same as calculations as cmv_1
+        center = [(x1 + x2 + x3) / 3, (y1 + y2 + y3) / 3]
+        def distance_exceeds_radius(x, y):
+            squared_distance = (x - center[0]) ** 2 + \
+                (y - center[1]) ** 2
+
+            if squared_distance > radius1 ** 2:
+                # True if the point cannot be contained
+                return True
+            return False
+        if distance_exceeds_radius(x1, y1) or distance_exceeds_radius(x2, y2) or distance_exceeds_radius(x3, y3):
+            return True
+    # Both conditions failed
+    return False
+
+def cmv_condition_13(points, a_pts, b_pts, radius1, radius2, numpoints):
     """
     There exists at least one set of three data points, separated by exactly A PTS and B PTS 
     consecutive intervening points, respectively, that cannot be contained within or on a 
@@ -14,31 +66,28 @@ def cmv_condition_8(points, a_pts, b_pts, radius1, radius2):
 
 
     Args:
-        points (list): coordinates
-        a_pts (int): distance A PTS
-        b_pts (int): distance B PTS
+        points (list): list of (x,y) coordinates
+        a_pts (int): number of intervening points
+        b_pts (int): number of intervening points
         radius1 (float): circle radius
         radius2 (float): circle radius, must be 0 ≤ RADIUS2
 
     Returns:
         True if conditions are met
     """
-    if len(points) < 5:
+    if numpoints < 5:
         return False
     if radius2 <= 0:
         return False
-    radius1_contained = False
-    radius2_contained = False
-    total_points = len(points)
-    for i in range(total_points-2):
-        (x1, y1), (x2, y2), (x3, y3) = points[i], points[i+1], points[i+2]
 
-        # The distances A PTS and B PTS are integers
-        dist1 = int(math.sqrt((x2 - x1)**2 + (y2 - y1)**2))
-        dist2 = int(math.sqrt((x3 - x2)**2 + (y3 - y2)**2))
+    for i in range(numpoints - a_pts - b_pts - 2):
+        p1 = points[i]
+        p2 = points[i+a_pts+1]
+        p3 = points[i+a_pts+b_pts+2]
 
-        if not (dist1 == a_pts and dist2 == b_pts):
-            continue
+        x1, y1 = p1[0], p1[1]
+        x2, y2 = p2[0], p2[1]
+        x3, y3 = p3[0], p3[1]
 
         # Same as calculations as cmv_1
         center = [(x1 + x2 + x3) / 3, (y1 + y2 + y3) / 3]
@@ -52,18 +101,8 @@ def cmv_condition_8(points, a_pts, b_pts, radius1, radius2):
                 return True
             return False
 
-        # If one
-        if (distance_exceeds_radius(x1, y1, radius1) or distance_exceeds_radius(x2, y2, radius1) or distance_exceeds_radius(x3, y3, radius1)):
-            radius1_contained = True
-        if (distance_exceeds_radius(x1, y1, radius2) or distance_exceeds_radius(x2, y2, radius2) or distance_exceeds_radius(x3, y3, radius2)):
-            radius2_contained = True
-
-        if radius1_contained == True and radius2_contained == True:
-            return True
+        if (not distance_exceeds_radius(x1, y1, radius2) and not distance_exceeds_radius(x2, y2, radius2) and not distance_exceeds_radius(x3, y3, radius2)):
+            return cmv_condition_8(points, a_pts, b_pts, radius1, numpoints)
+        
     return False
 
-
-points = [(1, 0), (3, 0), (5, 0), (-1, -2), (2, 1),
-          (2, 2), (3, 3), (-2, -2), (1, 1), (-1, -1)]
-r = cmv_condition_8(points, 2, 2, 1.5, 1)
-print(r)
