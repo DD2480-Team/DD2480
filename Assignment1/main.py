92cfe87df9a02d875c45fc633a85b311e1d626d1
YenChen <b06902047@ntu.edu.tw>
2022-01-31 16:31:01 +0100

    feat: added form_the_pum

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -521,6 +521,40 @@ def check_condition_14(point_array, E_PTS, F_PTS, area1, area2):
             return True
     return False
 
+def form_the_pum(cmv, lcm):
+    '''
+    CMV and LCM are combined to form PUM.
+
+    CMV: The CMV is a boolean vector whose elements have a one-to-one
+    correspondence with the launch interceptor conditions
+    LCM: The entries in the LCM represent the logical connectors to be used 
+    between pairs of LICs to determine the corresponding entry in the PUM
+
+    Args:
+        cmv (list): list of boolean value
+        lcm(2D-list): 15*15 matrix of "ANDD", "ORR", "NOTUSED"
+
+    Returns:
+        pum(2D-list): 15*15 matrix of boolean value
+
+    '''
+    pum = [[False for x in range(15)] for i in range(15)]
+    
+    for i in range(15):
+        for j in range(i, 15):
+            if lcm[i][j] == "ANDD":
+                if cmv[i] and cmv[j]:
+                    pum[i][j] = True
+                    pum[j][i] = True
+            elif lcm[i][j] == "ORR":
+                if cmv[i] or cmv[j]:
+                    pum[i][j] = True
+                    pum[j][i] = True
+            elif lcm[i][j] == "NOTUSED":
+                pum[i][j] = True
+                pum[j][i] = True
+    return pum
+
 
 if __name__ == "__main__":
     pass
