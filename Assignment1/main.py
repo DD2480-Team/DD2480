02df4001e58f1545e4c8e33977639068be8a7b2e
Gautam Manek <gautam007@e.ntu.edu.sg>
2022-02-02 04:20:22 +0800

    feat: added command line parsing

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,6 +1,37 @@
+from fuv import form_the_fuv
+from generate_datapoints import generate_LCM_matrix
 from parser import *
+from pum import form_the_pum
+import sys
+
+
+def parse_args(arg):
+    ua = str(arg).upper()
+    if "TRUE".startswith(ua):
+        return True
+    elif "FALSE".startswith(ua):
+        return False
+    else:
+        raise ArgumentError
+
+
+def launch(should_launch):
+    puv = [False for _ in range(15)] if should_launch else [True for _ in range(15)]
+    cmv = read_CMV_PUV_LCM_from_file()
+    print(f"The cmv vector is {cmv.CMV_VECTOR}")
+    vals = ["NOTUSED"] if should_launch else ["ORR"]
+    lcm = generate_LCM_matrix(vals)
+    print(f"The lcm matrix is {lcm}")
+    pum = form_the_pum(cmv.CMV_VECTOR, lcm)
+    print(f"The pum matrix is {pum}")
+    fuv = form_the_fuv(pum, puv)
+    print(f"The FUV vector is {fuv}")
+    if all([fuv[i] for i in range(len(fuv))]):
+        print("The rocket has been launched!")
+    else:
+        print("The rocket has not been launched")
 
 
 if __name__ == "__main__":
-    cmv = read_CMV_PUV_LCM_from_file()
-    print(cmv.CMV_VECTOR)
+    should_launch = sys.argv[1]
+    launch(parse_args(should_launch))
--- Assignment1/test_fuv.py
+++ Assignment1/test_fuv.py
@@ -6,7 +6,7 @@ from parser import *
 from generate_datapoints import *
 
 
-class FormCMVTestCase(unittest.TestCase):
+class FormFUVTestCase(unittest.TestCase):
     def test_successful_launch_1(self):
         puv = [False for _ in range(15)]
         cmv = read_CMV_PUV_LCM_from_file()
@@ -23,10 +23,16 @@ class FormCMVTestCase(unittest.TestCase):
         fuv = form_the_fuv(pum, puv)
         self.assertTrue(all([fuv[i] for i in range(len(fuv))]))
 
-    def test_failed_launch(self):
+    def test_failed_launch_1(self):
         puv = [True for _ in range(15)]
         cmv = read_CMV_PUV_LCM_from_file()
         lcm = generate_LCM_matrix(vals=["ORR"])
         pum = form_the_pum(cmv.CMV_VECTOR, lcm)
         fuv = form_the_fuv(pum, puv)
         self.assertFalse(all([fuv[i] for i in range(len(fuv))]))
+
+    # specifying an incorrect K_PTS = -1 causes
+    # the program to raise an assertion error
+    def test_failed_launch(self):
+        with self.assertRaises(AssertionError):
+            read_CMV_PUV_LCM_from_file("invalid_input.txt")
