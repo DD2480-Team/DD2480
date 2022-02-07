765b34813ad2b215ccde374613b33e7f27afeeab
Jacob Mimms <jacobmimms@gmail.com>
2022-02-07 18:11:57 +0100

    feat: issue #62 adding skeleton server that parses "push" webhooks from github added basic server that parses and prints to the terminal the user and email who pushed to the github repo

--- Assignment2/server.py
+++ Assignment2/server.py
@@ -1,16 +1,28 @@
-# gist source:
-# https://gist.github.com/gene1wood/118e075c2af3effc13cec799a5d3661d
-
 from __future__ import print_function
-import sys
 from flask import Flask, request
 import json
 
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''}, methods=['GET', 'HEAD', 'POST'])
-@app.route('/<path:path>', methods=['GET', 'HEAD', 'POST'])
-def show_payload(path):
-    print('Path : /{}'.format(path))
-    print(json.dumps({'form': request.form}), file=sys.stderr)
-    return "success"
+@app.route('/')
+def home_page():
+    return "HOME!"
+
+@app.route('/github', methods=['POST'])
+def webhook_message():
+    if request.method == "POST":
+        if request.headers['X-Github-Event']=='push':
+            info=json.dumps(request.json)
+            data = json.loads(info)
+            respository = data["repository"]
+            owner = respository["owner"]
+            name = owner["name"]
+            email = owner["email"]
+            message = "user: {} \nemail: {}".format(name,email)
+            print(message)
+        return "sucess"
+
+
+if __name__=='__main__':
+    app.secret_key = 'super secret key'
+    app.run(debug=True,  port=4567)
