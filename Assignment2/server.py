623b8899af597a563648d3cfbe53f5ec56ceaa96
dajamante <aimaiga2@gmail.com>
2022-02-10 09:17:14 +0100

    fix: catch non push events issue #80

--- Assignment2/server.py
+++ Assignment2/server.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import build
 from flask import Flask, request
 from git import Repo
 import json
@@ -6,36 +7,37 @@ import gitfunctions
 app = Flask(__name__)
 
 defaultBranch = "master"
-import build
 
-app = Flask(__name__)
-
-defaultBranch = "push_for_testing"
 
 @app.route('/')
 def home_page():
     return "HOME PAGE!"
 
+
 @app.route('/github', methods=['POST'])
 def webhook_message():
     if request.method == "POST":
-        if request.headers['X-Github-Event']=='push':
-            info=json.dumps(request.json)
+        if request.headers['X-Github-Event'] == 'push':
+            info = json.dumps(request.json)
             data = json.loads(info)
             respository = data["repository"]
             owner = respository["owner"]
             name = owner["name"]
             email = owner["email"]
-            message = "user: {} \nemail: {}".format(name,email)
+            message = "user: {} \nemail: {}".format(name, email)
             print(message)
-            #create a git repo object, from which you can change branches as you please
+            # create a git repo object, from which you can change branches as you please
             gitRepo = gitfunctions.GitRepo(defaultBranch)
-            syntaxCheck = build.SyntaxCheck(gitRepo.repoLocalPath + "Assignment2/server.py")
+            syntaxCheck = build.SyntaxCheck(
+                gitRepo.repoLocalPath + "Assignment2/server.py")
             if syntaxCheck.result == True:
                 return "success"
             else:
                 return "failure"
+        else:
+            return "Not a push event."
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.run(debug=True,  port=4567)
