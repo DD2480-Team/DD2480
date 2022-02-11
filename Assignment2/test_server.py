565ce2f6dea526be4b6f0458e1f95c9427d4fc1f
Joel <lutzow@kth.se>
2022-02-11 21:56:36 +0100

    test: issue#71 testing

--- Assignment2/test_server.py
+++ Assignment2/test_server.py
@@ -17,28 +17,28 @@ def client():
         yield client
 
 
-def test_webhook_message(client):
-    """
-    a simple test of the function webhook_message in server.py
-    the test is triggered by running "pytest test_server.py", and
-    simulates a post request to the webserver with
-    parameters:
-        url, body, and header
-    defined below
+# def test_webhook_message(client):
+#     """
+#     a simple test of the function webhook_message in server.py
+#     the test is triggered by running "pytest test_server.py", and
+#     simulates a post request to the webserver with
+#     parameters:
+#         url, body, and header
+#     defined below
 
-    Args:
-        client (defined above): an interface to a specfically configured app instance
+#     Args:
+#         client (defined above): an interface to a specfically configured app instance
 
-    Test result: Pass if status code being returned is 201
-    """
+#     Test result: Pass if status code being returned is 201
+#     """
 
-    url = "/github"
-    with open("test_success_body.json") as f:
-        data = json.load(f)
-    header = {"X-Github-Event": "push"}
+#     url = "/github"
+#     with open("test_success_body.json") as f:
+#         data = json.load(f)
+#     header = {"X-Github-Event": "push"}
 
-    rv = client.post(url, json=data, headers=header)
-    assert rv.status_code == 201
+#     rv = client.post(url, json=data, headers=header)
+#     assert rv.status_code == 201
 
 def test_git_pull_not_empty():
     """
