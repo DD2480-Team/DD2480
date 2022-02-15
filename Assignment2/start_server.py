ccc9a22ebd1b93015163edc64cb0a047e115cff0
Joel <lutzow@kth.se>
2022-02-15 20:18:07 +0100

    feat: issue #115 document how to run the server from top of repo

--- Assignment2/start_server.py
+++ Assignment2/start_server.py
@@ -1,3 +1,8 @@
+    """ Convenience file to launch the server from top of repo
+
+        Use like:
+            $ python3 start_server.py
+    """
 import src.python.server as server
 
 if __name__ == "__main__":
